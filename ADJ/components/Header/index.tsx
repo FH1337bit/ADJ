6666be2577c8c990ee8dcc8056c731babff47c55
FH1337bit <filip.h@1337bit.io>
2022-10-03 14:29:32 +0200

    feat: mobile responsivnes

--- ADJ/components/Header/index.tsx
+++ ADJ/components/Header/index.tsx
@@ -2,20 +2,21 @@
 import styles from "../Header/index.module.scss";
 import logo from "../../images/ADJ_Logo.png";
 import hamburger from "../../images/hamburger.png";
+import Image from "next/image";
 const Header = () => {
     return (
         <div className={styles.menu}>
             <div className={styles.navbar}>
                 <div className={styles.hamburger_box}>
-                    <img src={hamburger.src} style={{ width: "4vw", height: "5vh" }} alt="Hamburger" />
+                    <Image src={hamburger} className={styles.hamburger} width={40} height={30} alt="Hamburger" />
                     <label>MENU</label>
                 </div>
                 <div className={styles.joinus}>
                     <label>JOIN US</label>
                 </div>
             </div>
-            <div className={styles.logo}>
-                <img src={logo.src} style={{ width: "14vw", height: "23vh" }} alt="ADJ" />
+            <div className={styles.logo_box}>
+                <Image src={logo} className={styles.logo} alt="ADJ" />
                 <h1>Your partner in iGaming</h1>
                 <div className={styles.separator}></div>
                 <h2>Join our journey together in gaming world. </h2>
