b98183c021334f772ac1db453b9519a7592fb1c6
FH1337bit <filip.h@1337bit.io>
2022-10-03 16:24:36 +0200

    feat: test commit

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,22 +1,22 @@
 /* eslint-disable @next/next/no-img-element */
 import styles from "../Header/index.module.scss";
-import logo from "../../images/ADJ_Logo.png";
+import logo from "../../images/ADJ_logo.png";
 import hamburger from "../../images/hamburger.png";
-import Image from "next/image";
+
 const Header = () => {
     return (
         <div className={styles.menu}>
             <div className={styles.navbar}>
                 <div className={styles.hamburger_box}>
-                    <Image src={hamburger} className={styles.hamburger} width={40} height={30} alt="Hamburger" />
+                    <img src={hamburger.src} style={{ width: "4vw", height: "5vh" }} alt="Hamburger" />
                     <label>MENU</label>
                 </div>
                 <div className={styles.joinus}>
                     <label>JOIN US</label>
                 </div>
             </div>
-            <div className={styles.logo_box}>
-                <Image src={logo} className={styles.logo} alt="ADJ" />
+            <div className={styles.logo}>
+                <img src={logo.src} style={{ width: "14vw", height: "23vh" }} alt="ADJ" />
                 <h1>Your partner in iGaming</h1>
                 <div className={styles.separator}></div>
                 <h2>Join our journey together in gaming world. </h2>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,14 @@
 import Head from "next/head";
 import { Fragment } from "react";
+import About from "../components/About";
+import Company from "../components/Company";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import Quote from "../components/Quote";
+import ScrollText from "../components/ScrollText";
+import Team from "../components/Team";
+import Us from "../components/Us";
+import Work from "../components/Work";
 
 export default function Home() {
     return (
@@ -12,13 +19,10 @@ export default function Home() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <div className="content">
-                <div className="navbar">
+                <div className="header">
                     <Header></Header>
                 </div>
             </div>
-            <footer>
-                <Footer></Footer>
-            </footer>
         </Fragment>
     );
 }
