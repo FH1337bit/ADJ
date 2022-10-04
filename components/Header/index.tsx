2d7db2e1e77eaec86c32524eb94045dd6d6ce024
HardtVeevero <filip.hardt@veevero.cz>
2022-10-04 11:38:26 +0200

    fix: images

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,14 +1,15 @@
 /* eslint-disable @next/next/no-img-element */
 import styles from "../Header/index.module.scss";
-import logo from "../../images/ADJ_logo.png";
+import logo from "../../images/logo.png";
 import hamburger from "../../images/hamburger.png";
+import Image from "next/image";
 
 const Header = () => {
     return (
         <div className={styles.menu}>
             <div className={styles.navbar}>
                 <div className={styles.hamburger_box}>
-                    <img src={hamburger.src} style={{ width: "4vw", height: "5vh" }} alt="Hamburger" />
+                    <Image src={hamburger.src} style={{ width: "4vw", height: "5vh" }} alt="Hamburger" />
                     <label>MENU</label>
                 </div>
                 <div className={styles.joinus}>
@@ -16,7 +17,7 @@ const Header = () => {
                 </div>
             </div>
             <div className={styles.logo}>
-                <img src={logo.src} style={{ width: "14vw", height: "23vh" }} alt="ADJ" />
+                <Image src={logo.src} style={{ width: "14vw", height: "23vh" }} alt="ADJ" />
                 <h1>Your partner in iGaming</h1>
                 <div className={styles.separator}></div>
                 <h2>Join our journey together in gaming world. </h2>
